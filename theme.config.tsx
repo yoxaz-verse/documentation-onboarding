246c79e0a260cdabb86c0adfe9fac0cd6fa7977a
Jacob-alwin <alwinjacobjoy@gmail.com>
2025-08-04 01:36:52 +0530

    Add

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,7 +9,7 @@ const config: DocsThemeConfig = {
   // chat: {
   //   link: "https://discord.com",
   // },
-  // docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase: "https://www.obaol.com",
   footer: {
     text: "Obaol Sales Guide",
   },
