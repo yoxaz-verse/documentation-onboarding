26d64e4b7d89abe653603dd6be77fbda85a32ed0
Jacob-alwin <alwinjacobjoy@gmail.com>
2025-08-03 23:11:25 +0530

    Onboarding

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>OBAOL Supreme</span>,
+  // project: {
+  //   link: "https://www.instagram.com/obaol_supreme/",
+  // },
+  // chat: {
+  //   link: "https://discord.com",
+  // },
+  // docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  footer: {
+    text: "Obaol Sales Guide",
+  },
+};
+
+export default config;
